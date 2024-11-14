b42cb24ca12d5a4b3d79da088e41e54648709fd1
Blu <blu.medina.biz@gmail.com>
2024-11-14 13:08:49 -0500

    passed sum test

--- doctest/num_vector_utils.cpp
+++ doctest/num_vector_utils.cpp
@@ -7,6 +7,11 @@ string render_num_vector(const vector<int>&) {
     return s;
 }
 
-int num_vector_sum(const vector<int>&) {
-	return 0;
+int num_vector_sum(const vector<int>& v) {
+	int nums = 0;
+	for(int i = 0; i < sizeof(v); i++){
+		nums += v[i];
+} 	
+
+return nums;
 }
