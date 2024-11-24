58cafc5336aaf3d4ac8a6b60222250ad7d552114
Blu <blu.medina.biz@gmail.com>
2024-11-23 19:04:29 -0500

    test 1 fail - i dont want to talk about how long this took me

---
+++ doctest/ch11_s1/Time.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+using namespace std;
+
+
+struct Time {
+    //instance variables
+    int hour, minute, second;
+    
+    //constructors 
+    Time(int, int, int);
+    Time(int);
+    Time();
+
+    //functions
+    string toString();
+};
