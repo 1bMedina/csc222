eaded9bdad05d74f6f8214828761e56eae8d4c21
Blu <blu.medina.biz@gmail.com>
2024-12-18 19:34:23 -0500

    ive literally spent way too much time trying to get this to compile and it wont

---
+++ Chapter13/Card.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include "Card.h"
+using namespace std;
+
+
+Card::Card(Suit s, Rank r){
+}
+
+
+
+vector<string> suit_strings = {"None", "Clubs", "Diamonds",
+                                   "Hearts", "Spades"};
+vector<string> rank_strings = {"Joker", "2", "3", "4", "5", "6",
+                                   "7", "8", "9", "10", "Jack", "Queen",
+                                   "King", "Ace"};
+
+string Card::to_string() const {
+    if (rank == 0) return rank_strings[rank];
+    return rank_strings[rank] + " of " + suit_strings[suit];
+}
+
+bool Card::operator<(const Card& c) const{
+    return (rank == c.rank && suit == c.suit);
+}
+
+bool Card::operator>(const Card& c) const {
+    if(c.suit > suit) return false;
+    else if(c.suit < suit) return true;
+    else return rank > c.rank;
+}
+
+bool Card::operator<=(const Card& c) const{
+    return !(*this > c);
+}
+
+bool Card::operator>=(const Card& c) const{
+    return (*this > c) || (*this == c);
+}
+
+bool Card::operator==(const Card& c2) const {
+    return (rank == c2.rank && suit == c2.suit);
+}
+
+bool Card::operator!=(const Card& c2) const {
+    return !(this->operator==(c2));
+}
+
+vector<Card> build_deck()
+{
+    vector<Card> deck(52);
+    int i = 0;
+    for (Suit suit = CLUBS; suit <= SPADES; suit = Suit(suit+1)) {
+        for (Rank rank = TWO; rank <= ACE; rank = Rank(rank+1)) {
+            deck[i].suit = suit;
+            deck[i].rank = rank;
+            i++;
+        }
+    }
+    return deck;
+}
---
+++ Chapter13/test_card.cpp
@@ -0,0 +1,47 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+#include <iostream>
+#include <string>
+#include "Card.h"
+using namespace std;
+
+TEST_CASE("Test can create Cards") {
+    Card c1;
+    CHECK(c1.suit == 0);
+    CHECK(c1.rank == 0);
+    Card c2(3, 4);
+    CHECK(c2.suit == 3);
+    CHECK(c2.rank == 4);
+}
+
+TEST_CASE("Test can render Cards") {
+    Card c3(3, 12);
+    CHECK(c3.to_string() == "Queen of Hearts");
+    Card c4(2, 10);
+    CHECK(c4.to_string() == "10 of Diamonds");
+    Card c5;
+    CHECK(c5.to_string() == "Joker");
+}
+
+// TEST_CASE("Test comparison of Cards") {
+//     Card c1(2, 9);
+//     Card c2(1, 9);
+//     Card c3(1, 10);
+//     Card c4(1, 10);
+//     CHECK(c2 < c1 == true);
+//     CHECK(c3 <= c2 == false);
+//     CHECK(c1 >= c3 == true);
+//     CHECK(c3 == c4 == true);
+//     CHECK(c3 != c4 == false);
+// }
+
+TEST_CASE("Test can create and render Cards") {
+    Card c1(DIAMONDS, JACK);
+    CHECK(c1.to_string() == "Jack of Diamonds");
+    Card c2;
+    CHECK(c2.to_string() == "Joker");
+    Card c3(HEARTS, QUEEN);
+    CHECK(c3.to_string() == "Queen of Hearts");
+    Card c4(SPADES, SEVEN);
+    CHECK(c4.to_string() == "7 of Spades");
+}
